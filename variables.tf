713856a4b669c787e3244da3c37ea5072da03166
AwabIzzeldin <awabizzeldin02@gmail.com>
2025-07-03 19:24:00 +0800

    Delete variables.tf

--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "vpc_cidr" { default = "10.0.0.0/16" }
-variable "public_subnet_1_cidr" { default = "10.0.1.0/24" }
-variable "public_subnet_2_cidr" { default = "10.0.2.0/24" }
-variable "instance_type" { default = "t2.micro" }
-variable "db_username" { default = "admin" }
-variable "db_password" { default = "Secure123!" }
